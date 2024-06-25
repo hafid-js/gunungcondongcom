4743223f2cab50ea75197a92558860e3eeac6230
hafidjs <afidzpratama@gmail.com>
2024-06-25 07:07:22 +0700

    create category controller

--- src/main/java/com/hafidtech/gunungcondongcom/controller/CategoryController.java
+++ src/main/java/com/hafidtech/gunungcondongcom/controller/CategoryController.java
@@ -6,6 +6,7 @@ import com.hafidtech.gunungcondongcom.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,6 +18,8 @@ public class CategoryController {
 
     @GetMapping
     public PagedResponse<Category> getAllCategories(
-
+            @RequestParam(name = "page", required = false, defaultValue = AppConstants.DEFAULT_PAGE_NUMBER) Integer page,
+            @RequestParam(name = "size", required = false, defaultValue = AppConstants.DEFAULT_PAGE_SIZE) Integer size) {
+        return categoryService.getAllCategories(page, size);
     )
 }
