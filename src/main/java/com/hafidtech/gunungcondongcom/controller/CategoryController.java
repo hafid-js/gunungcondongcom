c3c2c09658608a256ed671c63ad5559e69dd8cb3
hafidjs <afidzpratama@gmail.com>
2024-06-25 07:09:22 +0700

    fixing

--- src/main/java/com/hafidtech/gunungcondongcom/controller/CategoryController.java
+++ src/main/java/com/hafidtech/gunungcondongcom/controller/CategoryController.java
@@ -2,12 +2,15 @@ package com.hafidtech.gunungcondongcom.controller;
 
 import com.hafidtech.gunungcondongcom.model.Category;
 import com.hafidtech.gunungcondongcom.payload.PagedResponse;
+import com.hafidtech.gunungcondongcom.security.CurrentUser;
+import com.hafidtech.gunungcondongcom.security.UserPrincipal;
 import com.hafidtech.gunungcondongcom.services.CategoryService;
+import com.hafidtech.gunungcondongcom.utils.AppConstants;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/categories")
@@ -21,5 +24,13 @@ public class CategoryController {
             @RequestParam(name = "page", required = false, defaultValue = AppConstants.DEFAULT_PAGE_NUMBER) Integer page,
             @RequestParam(name = "size", required = false, defaultValue = AppConstants.DEFAULT_PAGE_SIZE) Integer size) {
         return categoryService.getAllCategories(page, size);
-    )
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<Category> addCategory(@Valid @RequestBody Category category,
+                                                @CurrentUser UserPrincipal currentUser) {
+
+        return categoryService.addCategory(category, currentUser);
+    }
 }
