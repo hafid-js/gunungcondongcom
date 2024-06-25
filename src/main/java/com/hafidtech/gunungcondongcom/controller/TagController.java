5d366c159ddfac47641d51ef1efe0d6b442fbcf0
hafidjs <afidzpratama@gmail.com>
2024-06-25 15:40:09 +0700

    fix tag controller

--- src/main/java/com/hafidtech/gunungcondongcom/controller/TagController.java
+++ src/main/java/com/hafidtech/gunungcondongcom/controller/TagController.java
@@ -1,16 +1,18 @@
 package com.hafidtech.gunungcondongcom.controller;
 
 import com.hafidtech.gunungcondongcom.model.Tag;
+import com.hafidtech.gunungcondongcom.payload.ApiResponse;
 import com.hafidtech.gunungcondongcom.payload.PagedResponse;
+import com.hafidtech.gunungcondongcom.security.CurrentUser;
+import com.hafidtech.gunungcondongcom.security.UserPrincipal;
 import com.hafidtech.gunungcondongcom.services.TagService;
 import com.hafidtech.gunungcondongcom.utils.AppConstants;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/tags")
@@ -27,5 +29,46 @@ public class TagController {
         PagedResponse<Tag> response = tagService.getAllTags(page,size);
 
         return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<Tag> addTag(
+            @Valid
+            @RequestBody Tag tag,
+            @CurrentUser UserPrincipal currentUser) {
+        Tag newTag = tagService.addTag(tag, currentUser);
+
+        return new ResponseEntity<>(newTag, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Tag> getTag(
+            @PathVariable(name = "id") Long id) {
+        Tag tag = tagService.getTag(id);
+
+        return new ResponseEntity<>(tag, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    public ResponseEntity<Tag> updateTag(
+            @PathVariable(name = "id") Long id,
+            @Valid
+            @RequestBody Tag tag,
+            @CurrentUser UserPrincipal currentUser) {
+        Tag updateTag = tagService.updateTag(id, tag, currentUser);
+
+        return new ResponseEntity<>(updateTag, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    public ResponseEntity<ApiResponse> deleteTag(
+            @PathVariable(name = "id") Long id,
+            @CurrentUser UserPrincipal currentUser) {
+        ApiResponse apiResponse = tagService.deleteTag(id, currentUser);
+
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
     }
 }
